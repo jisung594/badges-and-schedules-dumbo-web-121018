e59e936e84669ebd150614bb729098da6f7f227d
jonathan choi <jisung594@gmail.com>
2018-11-25 23:04:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  return arr.map {|ele| badge_maker(ele)}
+end
+
+def assign_rooms(arr)
+  rooms = []
+  arr.each_with_index do |ele,i|
+    rooms << "Hello, #{ele}! You'll be assigned to room #{i + 1}!"
+  end
+  
+  return rooms
+end
+
+def printer
+  puts batch_badge_creator
+  
+  assign_rooms(arr).each
+end
+
